90ae84c5ca39d676e653cba7302e7ddfb5d80902
tunatech <ojobabatunde75@gmail.com>
2020-10-19 17:19:07 +0000

    Message board app

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
 class Post(models.Model):
-    text=models.TextField()
+    text = models.TextField()
 
     def __str__(self):
-        return (self.text[:50])
+        return self.text[:50]
